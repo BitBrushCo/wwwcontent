ac1e060986ef83c133341ef1cb1ad391d89c96ad
Md. Faisal Hossain <mdfhossain@ael-bd.com>
2016-08-01 16:53:55 +0600

    responsive in map section and work in contact section

--- js/main.js
+++ js/main.js
@@ -104,7 +104,24 @@ jQuery(function($) {
             keyboardNav: true,                            
         });
 
+	/***************** Google Map ******************/
 
+    function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(30.40964, -97.91923),
+			scrollwheel: false,
+			navigationControl: false,
+			mapTypeControl: false,
+			scaleControl: false,
+			draggable: false,
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 	//Presets
 	var presets = $('.style-chooser ul li');
